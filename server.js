30ab7124050f27e30025ee0708646212469d68f8
dwqn <dill02998@gmail.com>
2025-07-26 22:32:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const { Server } = require('socket.io');
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
 const io = new Server(http);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const users = new Map(); // socket.id -> { username, tag }
+const users = new Map();
 
 function generateTag(username) {
   let tag;
   do {
-    tag = Math.floor(1000 + Math.random() * 9000).toString();
+    tag = Math.floor(1000 + Math.random() * 9999).toString();
   } while ([...users.values()].some(u => u.username === username && u.tag === tag));
   return tag;
 }
 
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-  socket.on('register', (username) => {
+  socket.on("register", (username) => {
     const tag = generateTag(username);
-    users.set(socket.id, { username, tag });
-    io.emit('update-users', [...users.entries()].map(([id, u]) => ({ id, ...u })));
-    socket.emit('registered', { username, tag });
+    const user = { username, tag, id: socket.id };
+    users.set(socket.id, user);
+
+    socket.emit("registered", user);
+    io.emit("update-users", [...users.values()]);
   });
 
-  socket.on('send-message', ({ to, message }) => {
+  socket.on("send-message", ({ to, message }) => {
     const fromUser = users.get(socket.id);
     if (users.has(to)) {
-      io.to(to).emit('receive-message', {
+      io.to(to).emit("receive-message", {
         from: `${fromUser.username}#${fromUser.tag}`,
-        message
+        message,
       });
     }
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     users.delete(socket.id);
-    io.emit('update-users', [...users.entries()].map(([id, u]) => ({ id, ...u })));
+    io.emit("update-users", [...users.values()]);
   });
 });
 
 http.listen(3000, () => {
-  console.log('✅ Server listening on http://localhost:3000');
+  console.log("Server running: http://localhost:3000");
 });
