22e41bd6073df9937926e02f57f32574b01f8a55
dwqn <dill02998@gmail.com>
2025-07-27 10:26:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,31 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
-const http = require("http").createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(http);
+const PORT = 3000;
+
+const counterPath = path.join(__dirname, "visits.json");
 
 app.use(express.static("public"));
 
-const users = new Map();
+app.get("/", (req, res) => {
+  let count = 0;
 
-function generateTag(username) {
-  let tag;
-  do {
-    tag = Math.floor(1000 + Math.random() * 9999).toString();
-  } while ([...users.values()].some(u => u.username === username && u.tag === tag));
-  return tag;
-}
+  if (fs.existsSync(counterPath)) {
+    try {
+      const data = JSON.parse(fs.readFileSync(counterPath));
+      count = data.count || 0;
+    } catch (err) {}
+  }
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+  count++;
+  fs.writeFileSync(counterPath, JSON.stringify({ count }));
 
-  socket.on("register", (username) => {
-    const tag = generateTag(username);
-    const user = { username, tag, id: socket.id };
-    users.set(socket.id, user);
-
-    socket.emit("registered", user);
-    io.emit("update-users", [...users.values()]);
-  });
-
-  socket.on("send-message", ({ to, message }) => {
-    const fromUser = users.get(socket.id);
-    if (users.has(to)) {
-      io.to(to).emit("receive-message", {
-        from: `${fromUser.username}#${fromUser.tag}`,
-        message,
-      });
-    }
-  });
-
-  socket.on("disconnect", () => {
-    users.delete(socket.id);
-    io.emit("update-users", [...users.values()]);
-  });
+  const html = fs.readFileSync(path.join(__dirname, "public/index.html"), "utf8");
+  res.send(html.replace("{{VISIT_COUNT}}", count));
 });
 
-http.listen(3000, () => {
-  console.log("Server running: http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
 });
